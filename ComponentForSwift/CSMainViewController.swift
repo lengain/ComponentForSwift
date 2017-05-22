f28b5fe5f017a341d09d9fa6971143ef894ab32d
橙子 <xunzhaoyulong@163.com>
2017-05-22 10:37:27 +0800

    #语法:枚举

--- ComponentForSwift/CSMainViewController.swift
+++ ComponentForSwift/CSMainViewController.swift
@@ -33,22 +33,27 @@ class CSMainViewController: UIViewController,UITableViewDataSource,UITableViewDe
         self.view.addSubview(tableView)
         
         
-        dataSource = ["CSUIViewViewController","CSUILabelViewController"];
+        dataSource = [
+            ["CSEnumViewController"],
+            ["CSUIViewViewController","CSUILabelViewController"]
+        
+        ]
     }
 
     // MARK: - Delegate
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: CSMainViewController.UITableViewCellReuseId, for: indexPath)
-        cell.textLabel?.text = dataSource.object(at: indexPath.row) as? String
+        let sectionData : NSArray = dataSource.object(at: indexPath.section) as! NSArray
+        cell.textLabel?.text = sectionData.object(at: indexPath.row) as? String
         return cell;
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         tableView.deselectRow(at: indexPath, animated: true)
-        
-        let className = dataSource.object(at: indexPath.row) as! String
+        let sectionData : NSArray = dataSource.object(at: indexPath.section) as! NSArray
+        let className = sectionData.object(at: indexPath.row) as! String
         let customClass = NSClassFromString("ComponentForSwift." + className) as? UIViewController.Type;
         let vc:UIViewController = customClass!.init()
         self.navigationController?.pushViewController(vc, animated: true)
@@ -56,8 +61,34 @@ class CSMainViewController: UIViewController,UITableViewDataSource,UITableViewDe
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return dataSource.count;
+        let sectionData : NSArray = dataSource.object(at: section) as! NSArray
+        return sectionData.count
     }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return dataSource.count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 30;
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView.init(frame: CGRect.init(x: 0, y: 0, width: self.view.frame.size.width, height: 35))
+        let titleLabel = UILabel.init(frame: CGRect.init(x: 15, y: 0, width: 200, height: 35));
+        titleLabel.font = UIFont.systemFont(ofSize: 14)
+        switch section {
+        case 0:
+            titleLabel.text = "Grammer"
+        case 1 :
+            titleLabel.text = "UIKit"
+        default :
+            break
+        }
+        headerView.addSubview(titleLabel);
+        return headerView
+    }
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
---
+++ ComponentForSwift/Extension/CSEnumViewController.swift
@@ -0,0 +1,72 @@
+//
+//  CSEnumViewController.swift
+//  ComponentForSwift
+//
+//  Created by 橙子 on 2017/5/19.
+//  Copyright © 2017年 Orange. All rights reserved.
+//
+
+import UIKit
+
+enum CSDirection : Int{
+    case top = 0
+    case left
+    case bottom
+    case right
+}
+
+enum CSBarCode {
+    case upc(Int,Int,Int,Int)
+    case QRCode(String)
+}
+
+class CSEnumViewController: CSBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        let direction = CSDirection.top
+        switch direction {
+        case .top:
+            NSLog("direction.top")
+        default:
+            NSLog("direction.Others")
+        }
+        
+        var productBarcode = CSBarCode.upc(2, 4, 6, 8)
+        
+        switch productBarcode {
+        case .upc(let one, let two, let three, let four)://提取枚举值为常亮
+            print("UPC:\(one),\(two),\(three),\(four)")
+        default:
+            break;
+        }
+        
+        print(productBarcode)
+        productBarcode = CSBarCode.QRCode("1232145415")
+        switch productBarcode {
+        case let .upc(one, two, three, four)://如果一个枚举成员的所有关联值都被提取为常量，或者都被提取为变量，为了简洁，你可以只在成员名称前标注一个let或者var
+            print("UPC:\(one),\(two),\(three),\(four)")
+        case let .QRCode(codeString) :
+            print("QRCode:\(codeString)")
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
