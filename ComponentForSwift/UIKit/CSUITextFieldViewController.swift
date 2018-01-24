a3e1e046f9788b31b023055f9fb8ebe03578260e
大雄 <tongyulong@hzchengzi.com>
2018-01-24 14:32:42 +0800

    重命名

--- ComponentForSwift/UIKit/CSUITextFieldViewController.swift
+++ ComponentForSwift/UIKit/CSUITextFieldViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class CSUITextFidleViewController: UIViewController {
+class CSUITextFieldViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
